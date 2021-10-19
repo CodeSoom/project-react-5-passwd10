f1e9511c0bb971e8844823d464149bbc99151cbc
passwd10 <parkinseo94@naver.com>
2021-10-20 00:48:40 +0900

    test: new page 테스트를 수정한다

--- src/pages/__tests__/new.test.tsx
+++ src/pages/__tests__/new.test.tsx
@@ -3,25 +3,25 @@ import { render } from '@testing-library/react';
 import PostNewArticlePage from '../new.page';
 
 describe('HomePage', () => {
-  it('renders "글 작성하기"', () => {
+  it('renders title', () => {
     const { container } = render(<PostNewArticlePage />);
 
     expect(container).toHaveTextContent('글 작성하기');
   });
 
-  it('renders "제목"', () => {
+  it('renders title label', () => {
     const { container } = render(<PostNewArticlePage />);
 
     expect(container).toHaveTextContent('제목');
   });
 
-  it('renders "내용"', () => {
+  it('renders content label', () => {
     const { container } = render(<PostNewArticlePage />);
 
     expect(container).toHaveTextContent('내용');
   });
 
-  it('renders "게시하기"', () => {
+  it('renders submit button', () => {
     const { container } = render(<PostNewArticlePage />);
 
     expect(container).toHaveTextContent('게시하기');
