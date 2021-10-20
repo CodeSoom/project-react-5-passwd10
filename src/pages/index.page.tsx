4b5f998e4a36ce6a3c1557f50bb059db98d685bc
passwd10 <parkinseo94@naver.com>
2021-10-21 01:36:03 +0900

    feat: 홈페이지에 작성한글들의 제목이 보인다

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,56 @@
-export default function HomePage() {
+import { collection, getDocs } from 'firebase/firestore';
+
+import firestore from '../../firebase/clientApp';
+
+interface Post {
+  id: string;
+  content: string;
+  title: string;
+}
+
+interface Props {
+  posts: Post[]
+}
+
+export default function HomePage({ posts }: Props) {
   return (
     <div>
-      Hello World!
+      <h1>환영합니다</h1>
+      <ul>
+        {posts.map(({ id, title }) => (
+          <li key={id}>
+            제목:
+            {title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const querySnapshot = await getDocs(collection(firestore, 'posts'));
+  const posts: Post[] = [];
+
+  if (!querySnapshot) {
+    return {
+      notFound: true,
+    };
+  }
+
+  querySnapshot.forEach((doc) => {
+    const post: Post = {
+      id: doc.id,
+      content: doc.data().content,
+      title: doc.data().title,
+    };
+
+    posts.push(post);
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
