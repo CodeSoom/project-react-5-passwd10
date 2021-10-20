1d9ae587fa7a3a426fc4604473f8175fd7138a14
passwd10 <parkinseo94@naver.com>
2021-10-21 00:48:43 +0900

    feat: 테스트용 함수를 제거한다

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,21 +1,7 @@
-import { useRouter } from 'next/router';
-
 export default function HomePage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/new');
-  };
-
   return (
     <div>
       Hello World!
-      <button
-        type="button"
-        onClick={handleClick}
-      >
-        Go New Page
-      </button>
     </div>
   );
 }
